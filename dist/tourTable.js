43d6bba2d9a988a1ded91c5e529563802679f007
stipafk <stipafk@gmail.com>
2017-04-06 10:20:53 +0300

    ver 0.1.1
    set options and permission on click

--- dist/tourTable.js
+++ dist/tourTable.js
@@ -1,9 +1,13 @@
 class tourTable {
-	constructor(team,structure){
-		this.team = team;
-		this.structure = structure;
-		this.container = $('<div id="tourTable">');
-		this.renderTable(this.structure);
+	constructor(structure, options){
+		if(options){
+			this.options = options;
+		}
+		if(structure){
+			this.structure = structure;
+			this.container = $('<div id="tourTable">');
+			this.renderTable(this.structure);
+		}
 	}
 	setStructure(structure){
 		var that = this;
@@ -31,62 +35,64 @@ class tourTable {
 					match.find('.team-bottom').append('<span class="score">Winner</span>');
 				}
 
-				match.on('click', '.team', function(){
-					var round_id = $(this).parents('.round').data('round-id');
-					var match_id = $(this).parents('.matchup').data('match-id');
-					var team_id = $(this).data('team-id');
-					var countWiner = 0;
-					if($(this).hasClass('winner')){
-						that.structure[round_id][match_id].winner = '';
-					}else{
-						that.structure[round_id][match_id].winner = $(this).data('team-id');
-						for (let i = 0; i < that.structure[round_id].length; i++){
-							if (that.structure[round_id][i].winner){
-								countWiner++;
-							}
-						}
-						if(countWiner == that.structure[round_id].length && countWiner != 1){
-							that.structure[round_id+1] = [];
-							for (var i = round_id+1; i < that.structure.length; i++){ // чистим все следуйщие масивы от игр
-								that.structure[i] = [];
-							}
-							var allWinner = [];
-							for (var i = 0; i < countWiner; i++){ // добавляем победителей в масив
-								if(that.structure[round_id][i].winner == that.structure[round_id][i].team1id){
-									var team = {
-										id: that.structure[round_id][i].team1id,
-										name: that.structure[round_id][i].team1name
-									}
-									allWinner.push(team);
-								}else {
-									var team = {
-										id: that.structure[round_id][i].team2id,
-										name: that.structure[round_id][i].team2name
-									}
-									allWinner.push(team);
+				if(that.options && that.options.permission && that.options.permission == 'administrator' || that.options.permission == 'moderator'){
+					match.on('click', '.team', function(){
+						var round_id = $(this).parents('.round').data('round-id');
+						var match_id = $(this).parents('.matchup').data('match-id');
+						var team_id = $(this).data('team-id');
+						var countWiner = 0;
+						if($(this).hasClass('winner')){
+							that.structure[round_id][match_id].winner = '';
+						}else{
+							that.structure[round_id][match_id].winner = $(this).data('team-id');
+							for (let i = 0; i < that.structure[round_id].length; i++){
+								if (that.structure[round_id][i].winner){
+									countWiner++;
 								}
 							}
-							for(var i = 0; i < allWinner.length; i++){ // добавляем команду
-								var team = {
-									team1id: allWinner[i].id,
-									team1name: allWinner[i].name,
-									team2name: allWinner[i+1].name,
-									team2id: allWinner[i+1].id
+							if(countWiner == that.structure[round_id].length && countWiner != 1){
+								that.structure[round_id+1] = [];
+								for (var i = round_id+1; i < that.structure.length; i++){ // чистим все следуйщие масивы от игр
+									that.structure[i] = [];
 								}
-								i++;
-								that.structure[round_id+1].push(team)
+								var allWinner = [];
+								for (var i = 0; i < countWiner; i++){ // добавляем победителей в масив
+									if(that.structure[round_id][i].winner == that.structure[round_id][i].team1id){
+										var team = {
+											id: that.structure[round_id][i].team1id,
+											name: that.structure[round_id][i].team1name
+										}
+										allWinner.push(team);
+									}else {
+										var team = {
+											id: that.structure[round_id][i].team2id,
+											name: that.structure[round_id][i].team2name
+										}
+										allWinner.push(team);
+									}
+								}
+								for(var i = 0; i < allWinner.length; i++){ // добавляем команду
+									var team = {
+										team1id: allWinner[i].id,
+										team1name: allWinner[i].name,
+										team2name: allWinner[i+1].name,
+										team2id: allWinner[i+1].id
+									}
+									i++;
+									that.structure[round_id+1].push(team)
+								}
+							}
+							if(round_id != 0){
+								for (let i = 0; i < round_id; i++){
+									var round_id_change = that.container.find('.round-' + that.intToString(i)).find('li[data-team-id='+ team_id +']').parents('.round').data('round-id');
+									var match_id_change = that.container.find('.round-' + that.intToString(i)).find('li[data-team-id='+ team_id +']').parents('.matchup').data('match-id');
+									that.structure[round_id_change][match_id_change].winner = $(this).data('team-id');
+								}
 							}
 						}
-						if(round_id != 0){
-							for (let i = 0; i < round_id; i++){
-								var round_id_change = that.container.find('.round-' + that.intToString(i)).find('li[data-team-id='+ team_id +']').parents('.round').data('round-id');
-								var match_id_change = that.container.find('.round-' + that.intToString(i)).find('li[data-team-id='+ team_id +']').parents('.matchup').data('match-id');
-								that.structure[round_id_change][match_id_change].winner = $(this).data('team-id');
-							}
-						}
-					}
-					that.setStructure(that.structure);
-				});
+						that.setStructure(that.structure);
+					});
+				}
 
 				match.on('mouseover', '.team', function(){
 					var round_id = $(this).parents('.round').data('round-id');
